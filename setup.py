f003686d68d659314eaae7a4b49e4b517c8564cd
Andreas Baulig <free.geronimo@hotmail.de>
2018-09-29 17:08:14 +0200

    Add setup meta data

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='autofff',
-      version='0.1',
+      version='0.1_1',
       description='Auto-generate FFF fake definitions for C API header files',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/FreeGeronimo/autofff',
       author='Andreas Baulig',
       author_email='free.geronimo@hotmail.de',
       license='MIT',
       packages=['autofff'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
