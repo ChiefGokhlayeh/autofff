8aacbc57858024d6da289bb404ec8f42a0e49556
Andreas Baulig <free.geronimo@hotmail.de>
2018-09-29 18:07:06 +0200

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'r') as fh:
+    install_requires = fh.readlines()
+
 setup(name='autofff',
-      version='0.2',
-      description='Auto-generate FFF fake definitions for C API header files',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='https://github.com/FreeGeronimo/autofff',
-      author='Andreas Baulig',
-      author_email='free.geronimo@hotmail.de',
-      license='MIT',
-      packages=['autofff'],
-      zip_safe=False,
-      classifiers=[
+    version='0.2',
+    description='Auto-generate FFF fake definitions for C API header files',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/FreeGeronimo/autofff',
+    author='Andreas Baulig',
+    author_email='free.geronimo@hotmail.de',
+    license='MIT',
+    packages=['autofff'],
+    zip_safe=False,
+    classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ])
+    ],
+    install_requires=install_requires)
